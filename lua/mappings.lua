b026f8b0ec93209059c9c7c79e90945e9a3209e2
rafaelinaciodev <rafael.inacio.dev@gmail.com>
2024-07-28 21:29:10 -0300

    remove mapping for persistence session

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -47,16 +47,3 @@ map("n", "S", "<cmd> HopChar1 <CR>", { noremap = true, silent = true })
 
 -- Todo comments
 map("n", "tl", "<cmd> TodoTelescope <CR>")
-
--- Session
--- load the session for the current directory
-vim.keymap.set("n", "<leader>qs", function() require("persistence").load() end)
-
--- select a session to load
-vim.keymap.set("n", "<leader>qS", function() require("persistence").select() end)
-
--- load the last session
-vim.keymap.set("n", "<leader>ql", function() require("persistence").load({ last = true }) end)
-
--- stop Persistence => session won't be saved on exit
-vim.keymap.set("n", "<leader>qd", function() require("persistence").stop() end)
