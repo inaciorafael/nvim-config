910a8a41f755713a361e118f0593749276a972bc
Rafael Inácio <rafael.oliveira@modalgr.io>
2025-02-17 11:43:07 -0300

    feat: tasks

--- init.lua
+++ init.lua
@@ -22,3 +22,5 @@ require "plugins"
 
 -- vim.cmd "colorscheme nightfox"
 vim.cmd.colorscheme "everforest"
+
+-- TODO: Escrever função para mockar objetos com dados fake para desenvolvimento frontend.
