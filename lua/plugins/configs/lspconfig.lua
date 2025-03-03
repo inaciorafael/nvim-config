9adb968fa0c2898547106dc03c149a0011b5d5aa
rafaelinaciodev <rafael.inacio.dev@gmail.com>
2025-03-02 21:34:38 -0300

    fix: class name

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -57,7 +57,7 @@ lspconfig.lua_ls.setup {
 }
 
 -- setup multiple servers with same default options
-local servers = { "tsserver", "html", "cssls", "tailwindcss", "pyright", "astro", "angularls", "ts_ls" }
+local servers = { "tsserver", "html", "cssls", "tailwindcss", "pyright", "astro", "angularls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
