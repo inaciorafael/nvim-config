fc4ba7ffb7f538942784e92c96f9ff077569a815
rafael.oliveira <rafael.oliveira@familhao.digital>
2025-11-13 16:57:48 -0300

    feat: best config native lsp

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,46 +1,49 @@
-local M = {}
-
--- Carrega configs básicas
-local utils = require "lsp.utils"
-
--- LSPs disponíveis (basta criar um novo arquivo e adicionar aqui)
-local servers = {
-  lua_ls = require "lsp.servers.lua_ls",
-  ts_ls = require "lsp.servers.ts_ls",
-  pyright = require "lsp.servers.pyright",
+vim.lsp.config["lua_ls"] = {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_makers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
 }
 
--- Configuração global
-vim.lsp.config["*"] = {
-  capabilities = utils.capabilities,
-  on_attach = utils.on_attach,
+vim.lsp.config["tailwindcss"] = {
+  filetypes = { "typescriptreact", "javascriptreact", "html" },
 }
 
--- Registra servidores individualmente
-for name, config in pairs(servers) do
-  vim.lsp.config[name] = config
-end
+vim.lsp.enable "lua_ls"
+vim.lsp.enable "ts_ls"
+vim.lsp.enable "tailwindcss"
 
--- Inicializa automaticamente o LSP com base no filetype
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "lua", "python", "typescript", "javascript", "typescriptreact", "javascriptreact" },
-  callback = function()
-    local ft = vim.bo.filetype
-    local server_name
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    if ft == "lua" then
-      server_name = "lua_ls"
-    elseif ft == "python" then
-      server_name = "pyright"
-    else
-      server_name = "ts_ls"
-    end
+    local opts = { buffer = ev.buf }
 
-    local cfg = vim.lsp.config[server_name]
-    if cfg then
-      vim.lsp.start(cfg)
-    end
+    vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wl", function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
   end,
 })
-
-return M
