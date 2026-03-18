ce6fb02195f4b22c50aca19628511f8bd96716c5
Rafael Inacio <rafael.inacio.dev@gmail.com>
2026-03-18 20:27:32 -0300

    feat: ajuste nas dependencias

--- lua/commands.lua
+++ lua/commands.lua
@@ -90,11 +90,11 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 -- mason, write correct names only
 vim.api.nvim_create_user_command("MasonInstallAll", function()
-  vim.cmd "MasonInstall css-lsp html-lsp lua-language-server typescript-language-server stylua prettier tailwindcss-language-server emmet-language-server pyright black"
+  vim.cmd "MasonInstall css-lsp html-lsp lua-language-server typescript-language-server stylua prettier tailwindcss-language-server emmet-language-server pyright black vue-language-server"
 end, {})
 
 vim.api.nvim_create_user_command("TSInstallAll", function()
-  vim.cmd "TSInstall css yaml xml tsx json html lua javascript typescript markdown python"
+  vim.cmd "TSInstall css yaml xml tsx json html lua javascript typescript markdown python vue toml"
 end, {})
 
 vim.api.nvim_create_user_command("S", function()
