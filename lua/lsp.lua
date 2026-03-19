1be83ea837dfd7f96dc3f70cf2f5d3c12c590787
rafael.oliveira <rafael.oliveira@familhao.digital>
2026-03-19 11:42:14 -0300

    feat: atalho para diagnosticos

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -147,6 +147,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>cd", vim.diagnostic.open_float, opts)
     vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
